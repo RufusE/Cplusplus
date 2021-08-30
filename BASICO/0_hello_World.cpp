96f19eae0f8d8de8d0c6264084ca5e8588d99daa
RufusE <linuxrufus@gmail.com>
2021-08-29 21:02:03 -0300

    alteração

---
+++ BASICO/0_hello_World.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    cout<<"\nHello World\n";
+    cout<<"Hello World!\n";
+    return 0;
+
+}
+
+
+
+
