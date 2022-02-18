58681e635de1cffdfad94fcc3a86800169d85995
RufusE <linuxrufus@gmail.com>
2022-02-17 21:45:08 -0300

    .

---
+++ BASICO/05_varConst.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.1415   //constante global
+#define canal cout<<"Canal fessor bruno";
+
+int main(){
+
+    int vidas, tiros, life;  // Declarações multiplas
+
+/*
+    int vidas;
+    int tiros;
+    int life;
+
+    int vidas; int tiros; int life;
+*/
+
+    int a = 3, b = 500, c = 100;
+
+    cout<< pi <<"\n";
+    canal
+
+
+
+    return 0;
+
+}
+
+
+
+
