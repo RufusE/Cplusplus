b88290f5779fe9cc25f38c0263a73631b2c1885d
RufusE <linuxrufus@gmail.com>
2022-02-25 20:46:27 -0300

    .

---
+++ BASICO/08_inverte.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num = 10 , num2 = 10;
+    cout<< num <<"\n\n";
+
+    //invertendo
+    num = num *- 1;
+    cout<< -num <<"\n\n";
+    cout<< num << "\n\n";
+
+    num2 -= 10;
+    cout<< num2 <<"\n\n";
+
+    int num3 = 100;
+    num3 =- num3;
+    cout<< num3 <<"\n\n";
+
+
+
+
+
+
+    return 0;
+}
