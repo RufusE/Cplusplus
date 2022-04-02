e3ae5d04a6282778972fcc3973a0698226a6a40e
“RufusE” <linuxrufus@gmail.com>
2022-04-02 17:49:19 -0300

    .

---
+++ BASICO/15_while_infinito.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //Loop infinito !!!!!!
+
+    int cont;
+
+    cont =0;
+    while (1){
+        cout<<" . " << cont << "\n";
+        cont++;
+    }
+    cout<<"\nRotina finalizada\n";
+
+    return 0;
+}
