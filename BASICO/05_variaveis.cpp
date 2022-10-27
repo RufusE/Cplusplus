093f7e56eab41918c1c8251d62e1a987b7c8bb43
“RufusE” <linuxrufus@gmail.com>
2022-10-26 22:34:36 -0300

    .

---
+++ BASICO/05_variaveis.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define texto cout<<"Chamou o texto\n\n";
+
+int main(){
+
+    int vidas = 3;
+    int municao = 500;
+    int life = 100;
+    int a , b , c;
+
+    cout<< pi <<"\n"; 
+    texto    
+    
+
+
+
+return 0;
+
+}
