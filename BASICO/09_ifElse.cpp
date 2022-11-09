cdddfe0a446cd0f2ff6ef01485657a161741c0bf
“RufusE” <linuxrufus@gmail.com>
2022-11-08 21:17:11 -0300

    .

---
+++ BASICO/09_ifElse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    int num2 = 5;
+    int sol = 1;
+    char opc = 's';
+
+    if(sol){   //1 - verdadeiro   0 - false
+        cout<<"Vou ao clube\n";
+    }else{
+        cout<<"Vou ao cinema\n";
+    }
+
+    
+
+    return 0;
+}
